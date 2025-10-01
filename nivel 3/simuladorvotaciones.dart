a6289d07ef6fa470b20e8f52cb8f2f5d42b9f4b6
fonsito <ajhenriquez02@gmail.com>
2025-10-01 14:32:54 -0400

    avances

--- nivel 3/simuladorvotaciones.dart
+++ nivel 3/simuladorvotaciones.dart
@@ -3,3 +3,11 @@ Usa Map y List para registrar votos
 y mostrar resultados en tiempo real con barras ASCII.
 */
 
+import 'dart:io';
+
+List<>
+
+//reciben quien si voto y quien no
+Map<String, bool> votos = {
+
+};
