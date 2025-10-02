38e49ca131f7dae4b91fe4f4184f75f6f63a292b
fonsito <ajhenriquez02@gmail.com>
2025-10-02 12:23:11 -0400

    historias aleatorias

--- nivel 3/historias.dart
+++ nivel 3/historias.dart
@@ -1,6 +1,43 @@
 /* 
 * Generador de historias aleatorias
-! Combina listas de personajes, 
-! lugares y acciones para crear frases únicas cada vez.
+! Combina listas de personajes, lugares y acciones para crear frases únicas cada vez.
 */
 
+import 'dart:io';
+import 'dart:math';
+
+import '../utilidades.dart';
+
+List<String> personajes = [
+  'Batman',
+  'Spiderman',
+  'Iron Man',
+];
+
+List<String> lugares = [
+  'Ciudad gotica',
+  'New york',
+  'California',
+];
+
+List<String> acciones = [
+  'Combatir el crimen',
+  'Buscar justicia',
+  'Salvar personas',
+];
+
+String frasesMaker(){
+    final random = Random();
+
+    String personaje = personajes[random.nextInt(personajes.length)];
+    String lugar = lugares[random.nextInt(lugares.length)];
+    String accion = acciones[random.nextInt(acciones.length)];
+    
+    String oracion = '$personaje estaba en $lugar para lograr su objetivo de $accion';
+
+    return oracion;
+}
+void main() {
+    encabezado('Bienvenido a historias aleatorias', 45);
+        frasesMaker();
+}
